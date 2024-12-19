c93626b539b75d9b1418d08d485bfa901f55023e
eradesaymeric <erades.aymeric@outlook.fr>
2024-12-19 10:23:04 +0100

    Création fichier config ini

---
+++ config_ini.py
@@ -0,0 +1,13 @@
+import os
+
+USER_PREFERENCE = {
+    'utilisateur': "USER",
+    'camera': {
+        'num_camera1': 0,
+        'json_camera1_path': '',
+        'num_camera2': 1,
+        'json_camera2_path': '',
+    }
+
+} 
+
